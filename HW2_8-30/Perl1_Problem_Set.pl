dd0050c623b666b450cde873c00c32eaab08a9e5
Brian Smith <basmith@email.arizona.edu>
2015-09-02 13:17:27 -0700

    HW2

---
+++ HW2_8-30/Perl1_Problem_Set.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+print "Hellow Tucson\n";
+
+my $name = "Brian Smith";
+print "$name \n";
+
+print "This is a print statement in double quotes with\t tabs\t and\n newlines.\n I am $name.\n Math is fun 2*6=", 2*6, "\n";
+
+print 'This is a print statement in single quotes with\t tabs\t and\n newlines\n.  I am $name. Math is fun 2*6=', 2*6, '\n';
+
+print "\n"
